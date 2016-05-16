55c86e8cc8c715d14b8097cc84d86ef63aeb00de
David Björklund <david.bjorklund@gmail.com>
2016-05-16 23:20:24 +0200

    tests

---
+++ test.js
@@ -0,0 +1,92 @@
+import tape from 'tape-catch';
+import tapava from './lib';
+import parser from 'tap-parser';
+import concat from 'concat-stream';
+
+const runTest = (fn, onResult) => {
+  const test = tapava.createHarness();
+  test(fn);
+  test.createStream().pipe(parser(onResult));
+};
+
+tape('msg', t => {
+  const test = tapava.createHarness();
+  test('the message', tt => {});
+  test.createStream().pipe(concat({encoding: 'string'}, string => {
+    t.is(string.split('\n')[1], '# the message');
+    t.end();
+  }));
+});
+
+tape('throwing is failing', t => {
+  runTest(
+    tt => {
+      throw new Error('Foo bar');
+    },
+    result => {
+      t.notOk(result.ok);
+      t.is(result.fail, 1);
+      t.is(result.count, 1);
+      t.is(result.failures[0].name, 'Error: Foo bar');
+      t.end();
+    }
+  );
+});
+
+tape('t.end is not needed', t => {
+  const test = tapava.createHarness();
+
+  test(tt => {
+    tt.pass('pass');
+  });
+
+  const p = parser(result => {
+    t.ok(result.ok);
+    t.is(result.pass, 1);
+    t.is(result.count, 1);
+    t.end();
+  });
+
+  const s = test.createStream();
+  s.pipe(p);
+  setImmediate(() => {
+    s.end();
+  });
+});
+
+tape('promises', t => {
+  let isAsync = false;
+  runTest(
+    tt => {
+      return new Promise(resolve => {
+        tt.pass('blip blop');
+        setImmediate(() => {
+          isAsync = true;
+          resolve();
+        });
+      });
+    },
+    result => {
+      t.ok(isAsync);
+      t.equal(result.count, 1);
+      t.equal(result.pass, 1);
+      t.end();
+    }
+  );
+});
+
+tape('promises, errors', t => {
+  runTest(
+    tt => {
+      return new Promise(resolve => {
+        throw new Error('Heya');
+      });
+    },
+    result => {
+      t.notOk(result.ok);
+      t.equal(result.count, 1);
+      t.equal(result.fail, 1);
+      t.end();
+    }
+  );
+});
