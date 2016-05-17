ee0fb0856877d17e8ccad2596e98789737ad5c0b
David Björklund <david.bjorklund@gmail.com>
2016-05-18 01:28:55 +0200

    callback mode, add a couple of t-methods

--- test.js
+++ test.js
@@ -39,7 +39,7 @@ tape('throwing is failing', t => {
   );
 });
 
-tape('t.end is not needed', t => {
+tape('simple .pass()', t => {
   const test = tapava.createHarness();
 
   test(tt => {
@@ -135,6 +135,183 @@ tape('skip', t => {
   testToResult(test, result => {
     t.ok(result.ok);
     t.equal(result.count, 0);
+    t.end();
+  });
+});
+
+tape('t.plan()', t => {
+  runTest(
+    tt => {
+      tt.plan(2);
+      tt.pass('passing!');
+      tt.pass('passing2');
+    },
+    result => {
+      t.ok(result.ok);
+      t.equal(result.count, 2);
+      t.equal(result.pass, 2);
+      t.end();
+    }
+  );
+});
+
+tape('t.pass() / t.fail()', t => {
+  runTest(
+    tt => {
+      tt.pass('passing!');
+      tt.fail('noes');
+    },
+    result => {
+      t.notOk(result.ok);
+      t.equal(result.count, 2);
+      t.equal(result.pass, 1);
+      t.equal(result.fail, 1);
+      t.end();
+    }
+  );
+});
+
+tape('t.truthy() / t.falsy()', t => {
+  t.plan(6);
+
+  runTest(
+    tt => {
+      tt.truthy(1);
+      tt.falsy(0);
+    },
+    result => {
+      t.ok(result.ok);
+      t.equal(result.count, 2);
+      t.equal(result.pass, 2);
+    }
+  );
+
+  runTest(
+    tt => {
+      tt.truthy(0);
+      tt.falsy(1);
+    },
+    result => {
+      t.notOk(result.ok);
+      t.equal(result.count, 2);
+      t.equal(result.fail, 2);
+    }
+  );
+});
+
+tape('t.true() / t.false()', t => {
+  t.plan(6);
+
+  runTest(
+    tt => {
+      tt.true(true);
+      tt.false(false);
+    },
+    result => {
+      t.ok(result.ok);
+      t.equal(result.count, 2);
+      t.equal(result.pass, 2);
+    }
+  );
+
+  runTest(
+    tt => {
+      tt.true(1);
+      tt.false(0);
+    },
+    result => {
+      t.notOk(result.ok);
+      t.equal(result.count, 2);
+      t.equal(result.fail, 2);
+    }
+  );
+});
+
+tape('t.is() / t.not()', t => {
+  t.plan(6);
+
+  runTest(
+    tt => {
+      tt.is(1, 1);
+      tt.not(1, '1');
+    },
+    result => {
+      t.ok(result.ok);
+      t.equal(result.count, 2);
+      t.equal(result.pass, 2);
+    }
+  );
+
+  runTest(
+    tt => {
+      tt.is(1, '1');
+      tt.not(1, 1);
+    },
+    result => {
+      t.notOk(result.ok);
+      t.equal(result.count, 2);
+      t.equal(result.fail, 2);
+    }
+  );
+});
+
+tape('t.deepEqual() / t.notDeepEqual()', t => {
+  t.plan(6);
+
+  runTest(
+    tt => {
+      tt.deepEqual({foo: true}, {foo: true});
+      tt.notDeepEqual({foo: true}, {foo: false});
+    },
+    result => {
+      t.ok(result.ok);
+      t.equal(result.count, 2);
+      t.equal(result.pass, 2);
+    }
+  );
+
+  runTest(
+    tt => {
+      tt.deepEqual({foo: true}, {foo: false});
+      tt.notDeepEqual({foo: true}, {foo: true});
+    },
+    result => {
+      t.notOk(result.ok);
+      t.equal(result.count, 2);
+      t.equal(result.fail, 2);
+    }
+  );
+});
+
+tape('t.end() is not allowed', t => {
+  runTest(
+    tt => {
+      tt.end();
+    },
+    result => {
+      t.notOk(result.ok);
+      t.equal(result.count, 1);
+      t.equal(result.fail, 1);
+      t.end();
+    }
+  );
+});
+
+tape('callback mode', t => {
+  const test = tapava.cb.createHarness();
+
+  test(function (tt) {
+    // tt.ok should not be defined since that's a tape method
+    // and not a tapava method
+    tt.pass('yes!');
+    t.notOk(tt.ok);
+    tt.end();
+  });
+
+  testToResult(test, result => {
+    t.ok(result.ok);
+    t.equal(result.count, 1);
+    t.equal(result.pass, 1);
     t.end();
   });
 });
